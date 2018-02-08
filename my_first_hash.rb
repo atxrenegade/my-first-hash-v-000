37987f36f9e93a2a00b668fa588098d4f388b116
Harleigh Abel <harleighabel@gmail.com>
2018-02-08 03:40:35 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,5 +3,6 @@ my_hash = {"Rocket" => "cat", "George" => "fish"}
 end
 
 def shipping_manifest
+  shipping_manifest = {"whale bone corsets" => nil, "oil paintings" => nil, "porcelain vases" => nil}
   
 end  
