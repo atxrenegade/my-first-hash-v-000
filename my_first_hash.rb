1f50cf58dd042faebb0823ef31587ff82f951c94
Harleigh Abel <harleighabel@gmail.com>
2018-02-08 03:42:02 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,5 +4,8 @@ end
 
 def shipping_manifest
   shipping_manifest = {"whale bone corsets" => 2, "oil paintings" => 3, "porcelain vases" => 5}
-  
 end  
+def retrieval
+  shipping_manifest["oil paintings"]
+end  
+  
