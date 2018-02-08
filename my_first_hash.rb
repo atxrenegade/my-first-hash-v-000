37ec525d1a40f7b53406b67b511218c220987694
Harleigh Abel <harleighabel@gmail.com>
2018-02-08 03:33:09 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,18 +1,28 @@
 def my_hash
-  hash = {"user" => "Harleigh","laptop" => "Dell XPS 15z", "desktop" => "IMac 27 inch", "tablet" => "Samsung Galaxy Tab e", "smartphone" => "Blu Advance 5.0"}
+  hash = {"cat"=>"Rocket"}
 end
 
 def shipping_manifest
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-end
+  shipping_manifest = {
+    "whale bone corsets" => 5,
+    "oil paintings" => 3,
+    "porcelain vases" => 2}
+end   
 
 def retrieval
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["oil paintings"]
-end
+end  
 
 def adding
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  
+  shipping_manifest["whale bone corsets"] = 5
+  shipping_manifest["porcelain vases"] = 2 
+  shipping_manifest["oil paintings"] = 3
   shipping_manifest["pearl necklace"] = 1
   shipping_manifest
 end
+
+
+
+  
+  
